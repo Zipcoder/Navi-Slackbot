d76d4dd35d08baf7e6aae6dd49f205fb597339ec
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-25 17:11:58 -0400

    set git

--- src/scripts/history.py
+++ src/scripts/history.py
@@ -195,7 +195,7 @@ def get_history(channel_id):
 
 
 def push_to_git(file_list):
-    repo = Repo('/app')
+    repo = Repo('/app/Navi-Slackbot')
     commit_message = 'committing links'
     repo.index.add(file_list)
     repo.index.commit(commit_message)
